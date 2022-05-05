c960fc0630e81cf2979bbb5749dea87be92328ac
meher12 <meher.khemiri@gmail.com>
2022-05-05 17:03:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+# Implementation Angular 13 Form Validation with Submit using Reactive Forms Module and Bootstrap 4.
+
+The form has:
+- Full Name: required
+- Username: required, from 6 to 20 characters
+- Email: required, email format
+- Password: required, from 6 to 40 characters
+- Confirm Password: required, same as Password
+- Accept Terms Checkbox: required
+
+[![form13.png](https://i.postimg.cc/J0tdQk5T/form13.png)](https://postimg.cc/305B8W6p)
---
+++ src/app/app.component.ts
@@ -0,0 +1,96 @@
+import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
+import Validation from './utils/validation';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
+
+  Data: Array<any> = [
+    { name: 'Pear', value: 'pear' },
+    { name: 'Plum', value: 'plum' },
+    { name: 'Kiwi', value: 'kiwi' },
+    { name: 'Apple', value: 'apple' },
+    { name: 'Lime', value: 'lime' },
+  ];
+
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+      
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20)
+          ]
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        checkArray: this.formBuilder.array([], [Validators.required]),
+
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40)
+          ]
+        ],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')]
+      }
+    );
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  onCheckboxChange(e: any) {
+    const checkArray: FormArray = this.form.get('checkArray') as FormArray;
+    if (e.target.checked) {
+      checkArray.push(new FormControl(e.target.value));
+    } else {
+      let i: number = 0;
+      checkArray.controls.forEach((item: any) => {
+        if (item.value == e.target.value) {
+          checkArray.removeAt(i);
+          return;
+        }
+        i++;
+      });
+    }
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    //console.log(JSON.stringify(this.form.value, null, 2));
+    console.log(this.form.value);
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
+  }
+}
